7176c4ba3c4cdcdc30c83e1b1919bc222c2bb1c3
Baris Gungor <gungorbaris41@gmail.com>
2023-05-30 15:08:03 +0300

    change add

--- app/src/main/java/com/barisgungorr/MapsActivity.kt
+++ app/src/main/java/com/barisgungorr/MapsActivity.kt
@@ -1,6 +1,7 @@
 package com.barisgungorr
 
 import android.Manifest
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
@@ -23,13 +24,17 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.barisgungorr.travelbook.databinding.ActivityMaps4Binding
 import com.google.android.material.snackbar.Snackbar
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMapClickListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMaps4Binding
     private lateinit var locationManager : LocationManager // konum alma
     private lateinit var locationListener: LocationListener // konum alma
     private lateinit var permissionLauncher : ActivityResultLauncher<String>
+    private lateinit var sharedPreferences: SharedPreferences
+    private var trackBoolean : Boolean? = false
+    private var selectedLatidude : Double? = null
+    private  var selectedLongitude : Double? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,18 +48,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        sharedPreferences = this.getSharedPreferences("com.barisgungorr", MODE_PRIVATE)
+        selectedLatidude = 0.0
+        selectedLongitude = 0.0
+
     }
 
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.setOnMapClickListener(this)
+
         //casting-- ben bunun location service olduğundan eminim diyoruz
         locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
         locationListener = object  : LocationListener{
             override fun onLocationChanged(location: Location) {
-              // println("location: "+ location.toString())
-                val userLocation = LatLng(location.latitude,location.longitude)
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,15f))
+                trackBoolean = sharedPreferences.getBoolean("trackBoolean",false)
+                if (trackBoolean == false!!) {
+                    val userLocation = LatLng(location.latitude,location.longitude)
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,10f))
+                }
 
             }
 
@@ -74,6 +87,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         }else {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0f,locationListener) // lokasyon aldığımız kısım
+            val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (lastLocation != null){
+                val lastUserLocation = LatLng(lastLocation.latitude,lastLocation.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15f))
+            }
+            mMap.isMyLocationEnabled = true // konumu etkinleştirdik mi
 
         }
 
@@ -93,12 +112,28 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
                 // izin verildi
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0f,locationListener)
+                val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                if (lastLocation != null) {
+                    val lastUserLocation = LatLng(lastLocation.latitude,lastLocation.longitude)
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15f))
+                }
+                mMap.isMyLocationEnabled = true
+
 
             }else{
                 //reddedildi
                 Toast.makeText(this@MapsActivity," izne ihtiyacım var!",Toast.LENGTH_LONG).show()
             }
         }
+
+    }
+
+    override fun onMapClick(p0: LatLng) {
+        mMap.clear()
+        mMap.addMarker(MarkerOptions().position(p0))
+
+        selectedLatidude = p0.latitude
+        selectedLongitude = p0.longitude
 
     }
 
