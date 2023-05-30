2d5fbb5230d4fe7364c65e90cd830ed663ba0bb8
Baris Gungor <gungorbaris41@gmail.com>
2023-05-30 18:45:10 +0300

    ready to create project

--- app/src/main/java/com/barisgungorr/MapsActivity.kt
+++ app/src/main/java/com/barisgungorr/MapsActivity.kt
@@ -24,15 +24,15 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.barisgungorr.travelbook.databinding.ActivityMaps4Binding
 import com.google.android.material.snackbar.Snackbar
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMapClickListener {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMapClickListener {  // burada uzun tıkladığımızda ne olacağını söylediğimiz listener'ı ekledik
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMaps4Binding
-    private lateinit var locationManager : LocationManager // konum alma
-    private lateinit var locationListener: LocationListener // konum alma
-    private lateinit var permissionLauncher : ActivityResultLauncher<String>
-    private lateinit var sharedPreferences: SharedPreferences
-    private var trackBoolean : Boolean? = false
+    private lateinit var locationManager : LocationManager // konum almak için yapı 1 -konum yöneticisi
+    private lateinit var locationListener: LocationListener // konum alma yapı 2  - konum dinleyicisi
+    private lateinit var permissionLauncher : ActivityResultLauncher<String> // izin işlemleri -> kullanmasak bile bir register işlemi yapmamız gerekiyor
+    private lateinit var sharedPreferences: SharedPreferences // son konum kaydetmek için
+    private var trackBoolean : Boolean? = false // takip bulyını
     private var selectedLatidude : Double? = null
     private  var selectedLongitude : Double? = null
 
@@ -48,33 +48,41 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMapClic
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        sharedPreferences = this.getSharedPreferences("com.barisgungorr", MODE_PRIVATE)
+        sharedPreferences = this.getSharedPreferences("com.barisgungorr", MODE_PRIVATE)  // init işlemi
         selectedLatidude = 0.0
         selectedLongitude = 0.0
 
     }
 
-
-    override fun onMapReady(googleMap: GoogleMap) {
+    override fun onMapReady(googleMap: GoogleMap) { // harita hazır olduğunda çağrılan fonksiyon
         mMap = googleMap
-        mMap.setOnMapClickListener(this)
+        mMap.setOnMapClickListener(this)  // aktivite uygulayacak demeliyiz uzun tıklanınca ne olacak  <harita-listener> bağlantısı için gerekiyor
 
-        //casting-- ben bunun location service olduğundan eminim diyoruz
-        locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
-        locationListener = object  : LocationListener{
+        // latitude,longitude -> enlem ve boylam
+        //val sydney = LatLng(-34,0,151.0)  burayı eifel vs istediğimiz yeri ekleyebiliriz
+
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney)) -> bana bir pozisyon ver diyor kamera açıldığında beni buradan başlat diyoruz
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,10f)) -> zoom' işlemi yaparak istediğimiz yere daha yakın bir başlangıç yaptırabiliriz
+        //mMap.addMarker(MarkerOptions().position(sydney).title("Marker in sydney"))
+
+        //** KULLANICI KONUM İŞLEMLERİ
+
+        locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager  // init ediyoruz konum yöneticisini (as diyerek casting ediyoruz bundan eminim)
+
+        locationListener = object  : LocationListener{  // init ediyoruz konum lisınırını
             override fun onLocationChanged(location: Location) {
-                trackBoolean = sharedPreferences.getBoolean("trackBoolean",false)
+                trackBoolean = sharedPreferences.getBoolean("trackBoolean",false)  //daha önce kaydedilmiş bir veri var mı kontrol etmek için kullanıyoruz yok ise
+
                 if (trackBoolean == false!!) {
                     val userLocation = LatLng(location.latitude,location.longitude)
                     mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,10f))
+                    sharedPreferences.edit().putBoolean("trackBoolean",true).apply() // tekrar çağrılıyor ve değeri true yani bir konum alındıysa tekrar çağrılmıyor
                 }
-
             }
-
         }
-        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) { // önceki vers.Uyumluluğu için contexCompat
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
-                Snackbar.make(binding.root,"İzne ihtiyacım var ",Snackbar.LENGTH_INDEFINITE).setAction("ihtiyacım var") {
+                Snackbar.make(binding.root,"İzne ihtiyacım var ",Snackbar.LENGTH_INDEFINITE).setAction("İzne ihtiyacım var !") {
                     //izin iste
                     permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
 
@@ -86,11 +94,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMapClic
             }
 
         }else {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0f,locationListener) // lokasyon aldığımız kısım
-            val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0f,locationListener) // lokasyon aldığımız kısım cihazı yorar güncellene
+                                                                                                                            //saniyede 1 için 1000 , mesafe için distance 10metre
+            val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER) //son bilinen konum
             if (lastLocation != null){
-                val lastUserLocation = LatLng(lastLocation.latitude,lastLocation.longitude)
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15f))
+                val lastUserLocation = LatLng(lastLocation.latitude,lastLocation.longitude) // konumun daha önce alınmama ihtimaline karşı bir kontrol yaptık
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,10f))
             }
             mMap.isMyLocationEnabled = true // konumu etkinleştirdik mi
 
@@ -109,7 +118,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMapClic
 
         permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {result ->
             if (result) {
-                if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) // izin verildiyse ==
                 // izin verildi
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0f,locationListener)
                 val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
@@ -117,7 +126,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMapClic
                     val lastUserLocation = LatLng(lastLocation.latitude,lastLocation.longitude)
                     mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15f))
                 }
-                mMap.isMyLocationEnabled = true
+                mMap.isMyLocationEnabled = true // konumu etkinleştirdik mi
 
 
             }else{
@@ -128,11 +137,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMapClic
 
     }
 
-    override fun onMapClick(p0: LatLng) {
-        mMap.clear()
-        mMap.addMarker(MarkerOptions().position(p0))
+    override fun onMapClick(p0: LatLng) {  // uzun tıkladığımızda ne olacak fonksiyonu
+        mMap.clear() // eski markerları silmek için kullanıyoruz
+        mMap.addMarker(MarkerOptions().position(p0))  // marker ekleme
 
-        selectedLatidude = p0.latitude
+        selectedLatidude = p0.latitude  // kayıt için enlem ve boylamı ayrı değişkenlere atadık
         selectedLongitude = p0.longitude
 
     }
